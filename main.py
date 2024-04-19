d007c1ce99fe9d45ea7797300c3ed2307fcf8b36
Abhinav <abhinavsingh12347@gmail.com>
2024-04-19 11:29:17 +0530

    added news feature

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import wikipedia
 import pywhatkit as wk
 import pyjokes
 import time
+import json
 
 engine = pyttsx3.init("sapi5")
 voices = engine.getProperty("voices")
@@ -30,6 +31,56 @@ def wishMe():
     else:
         speak("Good Evening!")
     speak("I am your Nexus AI. How may I help you Abhinav?")
+
+
+def latestnews():
+    api_dict = {
+        "business": "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=9006914aee56454884fa88bf26bcccc2",
+        "entertainment": "https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=9006914aee56454884fa88bf26bcccc2",
+        "health": "https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=9006914aee56454884fa88bf26bcccc2",
+        "science": "https://newsapi.org/v2/top-headlines?country=in&category=science&apiKey=9006914aee56454884fa88bf26bcccc2",
+        "sports": "https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=9006914aee56454884fa88bf26bcccc2",
+        "technology": "https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=9006914aee56454884fa88bf26bcccc2"
+    }
+
+    content = None
+    url = None
+    print("Which field news do you want, [business] , [health] , [technology], [sports] , [entertainment] , "
+        "[science]")
+    speak(
+        "Which field news do you want, [business] , [health] , [technology], [sports] , [entertainment] , "
+        "[science]")
+    field = takeCommand().lower()
+    for key, value in api_dict.items():
+        if key.lower() in field.lower():
+            url = value
+            print(url)
+            print("url was found")
+            break
+        else:
+            url = True
+    if url is True:
+        print("url not found")
+
+    news = requests.get(url).text
+    news = json.loads(news)
+    speak("Here is the first news.")
+
+    arts = news["articles"]
+    for articles in arts:
+        article = articles["title"]
+        print(article)
+        speak(article)
+        news_url = articles["url"]
+        print(f"for more info visit: {news_url}")
+        speak("Do you want me to continue?")
+        a = takeCommand().lower()
+        if str(a) == "continue":
+            pass
+        elif str(a) == "stop":
+            break
+
+    speak("that's all")
 
 
 def takeCommand():
@@ -243,6 +294,9 @@ if __name__ == "__main__":
                         speak("Failed to fetch weather forecast. Please try again later.")
                 else:
                     speak("Failed to recognize location. Please try again.")
+        elif "latest news" in query:
+            latestnews()
+
 
         elif "exit" in query:
             speak("Thank you for using Nexus. Have a good day Abhinav!")
